1477314ca74a387131538898477363ce160747de
Opozo2 <ospofe.developer02@gmail.com>
2023-11-28 22:04:34 +0100

    message date and hour format < 10 corrected

--- app/src/main/java/com/example/realtimechat/domain/usecases/SendMessageUseCase.kt
+++ app/src/main/java/com/example/realtimechat/domain/usecases/SendMessageUseCase.kt
@@ -13,15 +13,38 @@ class SendMessageUseCase @Inject constructor(
         val calendar = Calendar.getInstance()
         val userDto = UserDto(userName, false)
 
-        calendar.apply {
-            val messageDto = MessageDto(
-                msg = msg,
-                hour = "${get(Calendar.HOUR_OF_DAY) + 1}:${get(Calendar.MINUTE)}",
-                date = "${get(Calendar.YEAR)}/${get(Calendar.MONTH) + 1}/${get(Calendar.DAY_OF_MONTH)}",
-                user = userDto
-            )
+        val hour = getCorrectHour(calendar)
+        val date = getCorrectDate(calendar)
 
-            firebaseChatService.sendMsgToFirebase(messageDto = messageDto)
-        }
+        val messageDto = MessageDto(
+            msg = msg,
+            hour = hour,
+            date = date,
+            user = userDto
+        )
+
+        firebaseChatService.sendMsgToFirebase(messageDto = messageDto)
+
+    }
+
+    private fun getCorrectHour(calendar: Calendar): String {
+        val hour = calendar.get(Calendar.HOUR_OF_DAY) + 1
+        val minute = calendar.get(Calendar.MINUTE)
+
+        val formattedHour = if (hour < 10) String.format("0%d", hour) else hour.toString()
+        val formattedMinute = if (minute < 10) String.format("0%d", minute) else minute.toString()
+
+        return String.format("%s:%s", formattedHour, formattedMinute)
+    }
+
+    private fun getCorrectDate(calendar: Calendar): String {
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        val formattedMonth = if (month < 10) String.format("0%d", month) else month.toString()
+        val formattedDay = if (day < 10) String.format("0%d", day) else day.toString()
+
+        return String.format("%d/%s/%s", year, formattedMonth, formattedDay)
     }
 }
